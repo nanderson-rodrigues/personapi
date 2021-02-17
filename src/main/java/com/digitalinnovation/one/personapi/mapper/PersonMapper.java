4f8557da31be63892e61175f3c99bad8a9ceb797
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-02-17 17:20:37 -0300

    creates the mapper class PersonMapper mapping Person and PersonDTO

---
+++ src/main/java/com/digitalinnovation/one/personapi/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.digitalinnovation.one.personapi.mapper;
+
+import com.digitalinnovation.one.personapi.dto.request.PersonDTO;
+import com.digitalinnovation.one.personapi.entity.Person;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+    
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
