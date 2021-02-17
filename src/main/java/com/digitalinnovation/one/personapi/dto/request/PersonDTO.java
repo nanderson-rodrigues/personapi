f173f676fd693d9e4725ef395c560a6f103cc291
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-02-17 12:26:04 -0300

    creates DTO class PersonDTO

---
+++ src/main/java/com/digitalinnovation/one/personapi/dto/request/PersonDTO.java
@@ -0,0 +1,27 @@
+package com.digitalinnovation.one.personapi.dto.request;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+    
+    private Long id;
+
+    private String firstName;
+    
+    private String lastName;
+    
+    private String cpf;
+
+    private String birthDate;
+
+    private List<PhoneDTO> phones;
+}
