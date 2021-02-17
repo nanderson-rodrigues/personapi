8c724cad5232cb525b91e725357ec297bcb66237
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-02-17 17:22:40 -0300

    adds the personMapper instance in the class PersonService

--- src/main/java/com/digitalinnovation/one/personapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/one/personapi/service/PersonService.java
@@ -1,10 +1,13 @@
 
 package com.digitalinnovation.one.personapi.service;
 
+import com.digitalinnovation.one.personapi.dto.request.PersonDTO;
 import com.digitalinnovation.one.personapi.dto.response.MessageResponseDTO;
 import com.digitalinnovation.one.personapi.entity.Person;
+import com.digitalinnovation.one.personapi.mapper.PersonMapper;
 import com.digitalinnovation.one.personapi.repository.PersonRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +15,18 @@ public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;    
+
+    @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder()
             .message("Created Person with ID " + savedPerson.getId())
             .build();
