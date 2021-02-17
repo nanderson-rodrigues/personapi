8e3f83c6e62f165d6bda395eff251c18ec0ebafc
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-02-17 17:14:56 -0300

    adds annotations for the class PersonDTO

--- src/main/java/com/digitalinnovation/one/personapi/dto/request/PersonDTO.java
+++ src/main/java/com/digitalinnovation/one/personapi/dto/request/PersonDTO.java
@@ -2,6 +2,12 @@ package com.digitalinnovation.one.personapi.dto.request;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +21,21 @@ public class PersonDTO {
     
     private Long id;
 
+    @NotEmpty
+    @Size(min = 2, max = 100)
     private String firstName;
     
+    @NotEmpty
+    @Size(min = 2, max = 100)
     private String lastName;
     
+    @NotEmpty
+    @CPF
     private String cpf;
 
     private String birthDate;
 
+    @Valid
+    @NotEmpty
     private List<PhoneDTO> phones;
 }
