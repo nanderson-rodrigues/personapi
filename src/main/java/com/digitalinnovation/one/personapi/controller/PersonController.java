304325c8903962a8a2a8163662d2980a67aa8a3f
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-02-19 20:23:54 -0300

    creates get method findById on class PersonController

--- src/main/java/com/digitalinnovation/one/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/one/personapi/controller/PersonController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,11 @@ public class PersonController {
     @GetMapping
     public List<PersonDTO> listAll() {
         return personService.listAll();
+    }
+    
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) {
+        return personService.findById(id);
     }
 
 }
