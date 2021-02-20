ffa7607cdfcea379c97f1c17df5218fb6ee25828
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-02-19 22:47:46 -0300

    creates method updateById on class PersonController

--- src/main/java/com/digitalinnovation/one/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/one/personapi/controller/PersonController.java
@@ -13,9 +13,11 @@ import com.digitalinnovation.one.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,7 +49,13 @@ public class PersonController {
         return personService.findById(id);  
     }
 
-    @GetMapping("/{id}")
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO)
+            throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
         personService.delete(id);
