37e2b60ea6097fdf74287a23ad3cc820275c5d8c
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-02-19 22:21:00 -0300

    creates method deleteById on class PersonController

--- src/main/java/com/digitalinnovation/one/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/one/personapi/controller/PersonController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import com.digitalinnovation.one.personapi.dto.request.PersonDTO;
 import com.digitalinnovation.one.personapi.dto.response.MessageResponseDTO;
+import com.digitalinnovation.one.personapi.exceptions.PersonNotFoundException;
 import com.digitalinnovation.one.personapi.service.PersonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,15 +36,21 @@ public class PersonController {
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return this.personService.createPerson(personDTO);
     }
-    
+
     @GetMapping
     public List<PersonDTO> listAll() {
         return personService.listAll();
     }
-    
+
     @GetMapping("/{id}")
-    public PersonDTO findById(@PathVariable Long id) {
-        return personService.findById(id);
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);  
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
     }
 
 }
